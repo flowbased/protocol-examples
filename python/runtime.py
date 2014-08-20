61f62dda8ebc89fa9471e80dfe6a08fae4e903ce
Jon Nordby <jononor@gmail.com>
2014-08-20 17:01:43 +0200

    python: Fix runtime example, can now be used with NoFlo UI

--- python/runtime.py
+++ python/runtime.py
@@ -5,50 +5,97 @@ import random
 import gevent
 import geventwebsocket
 
-class Runtime(object):
-    """Trivial example of how one can represent an FBP-ish runtime"""
+class DummyRuntime(object):
+    """Trivial example of how one can represent an FBP-ish/dataflow runtime"""
     
     # Component library
     components = {
-        "MyComponent": {
-            'description': "An example component which does exactly nothing"
+        "DummyComponent": {
+            'description': "An example component which does exactly nothing",
             'inPorts': [
                 {'id': 'portA', 'type': 'boolean', 'description': 'A boolean port',
-                    'addressable': false, 'required': true  },
-                {'id': 'portB', 'type': ''}
-            ]
-        }
+                    'addressable': False, 'required': True  },
+                {'id': 'portB', 'type': 'any'}
+            ],
+            'outPorts': [
+                {'id': 'out1', 'type': 'string'}
+            ],
+        },
     }
 
     def __init__(self):
         # A runtime can have multiple graphs
-        # Normally only one toplevel / "main" can be ran, and the other "subgraphs" can be used as components
+        # Normally only one toplevel / "main" can be ran,
+        # and the other "subgraphs" can be used as components
         self.graphs = {}
 
+    def new_graph(self, graph_id):
+        self.graphs[graph_id] = {
+            'nodes': {},
+            'connections': [],
+            'iips': []
+        }
 
+    def add_node(self, graph_id, node_id, component_id):
+        # Normally you'd instantiate the component here,
+        # we just store the name
+        nodes = self.graphs[graph_id]['nodes']
+        nodes[node_id] = component_id
+    def remove_node(self, graph_id, node_id):
+        # Destroy node instance
+        nodes = self.graphs[graph_id]['nodes']
+        del nodes[node_id]
+    def add_edge(self, graph_id, src, tgt):
+        connections = self.graphs[graph_id]['connections']
+        connections.append((src, tgt))
+    def remove_edge(self, graph_id, src, tgt):
+        connections = self.graphs[graph_id]['connections']
+        connections = filter(lambda conn: conn[0] != src and conn[1] != tgt, connections)
+    def add_iip(self, graph_id, src, data):
+        iips = self.graphs[graph_id]['iips']
+        iips.append((src, data))
+    def remove_iip(self, graph_id, src):
+        iips = self.graphs[graph_id]['iips']
+        iips = filter(lambda iip: iip[0] != src, iips)
 
-class RuntimeProtocol(geventwebsocket.WebSocketApplication):
+class RuntimeApplication(geventwebsocket.WebSocketApplication):
 
     def __init__(self, runtime):
-        self.runtime = runtime
+        super(RuntimeApplication, self).__init__(self)
+        self.runtime = DummyRuntime()
 
     ### WebSocket transport handling ###
-    def on_open(self):send
-        print "Connection opened"
+    @staticmethod
+    def protocol_name():
+        # WebSocket sub-protocol
+        return 'noflo'
+
+    def on_open(self):
+        print "INFO: Connection opened"
 
     def on_close(self, reason):
-        print "Connection closed", reason
+        print "INFO: Connection closed. Reason:", reason
 
     def on_message(self, message):
+        if not message:
+            print 'WARN: got empty message'
+            return
+
         m = json.loads(message)
         dispatch = {
-            'runtime': self.handle_runtime
-            'component': self.handle_runtime
-            'graph': self.handle_graph
-            'network': self.handle_network
+            'runtime': self.handle_runtime,
+            'component': self.handle_component,
+            'graph': self.handle_graph,
+            'network': self.handle_network,
         }
-        try
-            dispatch[m.get(protocol)](m['command'], m['payload'])
+
+        handler = None
+        try:
+            handler = dispatch[m.get('protocol')]
+        except KeyError, e:
+            print "WARN: Subprotocol '%s' not supported" % (p)
+        else:
+            handler(m['command'], m['payload'])
 
     def send(self, protocol, command, payload):
         """Send a message to UI/client""" 
@@ -60,14 +107,14 @@ class RuntimeProtocol(geventwebsocket.WebSocketApplication):
 
         # Absolute minimum: be able to tell UI info about runtime and supported capabilities
         if command == 'getruntime':
-            pay = {
-                'type': 'fbp-python-example'
-                'version': '0.4' # protocol version
-                'capabilities': [ 
-                    'protocol:component'
-                ]
+            payload = {
+                'type': 'fbp-python-example',
+                'version': '0.4', # protocol version
+                'capabilities': [
+                    'protocol:component',
+                ],
             }
-            self.send('runtime', 'runtime', response)
+            self.send('runtime', 'runtime', payload)
         else:
             print "WARN: Unknown command '%s' for protocol '%s' " % (command, 'runtime')
 
@@ -76,59 +123,67 @@ class RuntimeProtocol(geventwebsocket.WebSocketApplication):
         # Practical minimum: be able to tell UI which components are available
         # This allows them to be listed, added, removed and connected together in the UI
         if command == 'list':
-            for component_name, component_data in self.runtime.components:
-                # Normally you'd have to do a bit of mapping between your internal/native representation
-                # of a component, and what the FBP protocol expects. We cheated and chose naming conventions that matched
-
-                self.send
+            for component_name, component_data in self.runtime.components.items():
+                # Normally you'd have to do a bit of mapping between
+                # your internal/native representation of a component,
+                # and what the FBP protocol expects.
+                # We cheated and chose naming conventions that matched
+                payload = component_data
+                payload['name'] = component_name
+                self.send('component', 'component', payload)
 
         else:
-            print "WARN: Unknown command '%s' for protocol '%s' " % (command, 'runtime')
+            print "WARN: Unknown command '%s' for protocol '%s' " % (command, 'component')
 
     def handle_graph(self, command, payload):
 
-        # Modify our graph representation to match that of the UI
-        # Note: if it is possible for the graph state to be changed by other things than the UI
-        # you must send a message on the same format, informing the UI about the change
+        # Modify our graph representation to match that of the UI/client
+        # Note: if it is possible for the graph state to be changed by other things than the client
+        # you must send a message on the same format, informing the client about the change
+        # Normally done using signals,observer-pattern or similar
 
-        graph = self.graphs.get(payload['graph'], None)
+        send_ack = True
 
         # New graph
         if command == 'clear':
-            self.graphs[payload['id']] = {
-                nodes: {},
-                connections: []
-        }
-
+            self.runtime.new_graph(payload['id'])
         # Nodes
         elif command == 'addnode':
-            # This is where you'd normally instantiate your component. We just store the name
-            graph['nodes'][payload['id']] = payload['component']
+            self.runtime.add_node(payload['graph'], payload['id'], payload['component'])
         elif command == 'removenode':
-            # Destroy your instance
-            del graph['nodes'][payload['id']
-
+            self.runtime.remove_node(payload['graph'], payload['id'])
         # Edges/connections
         elif command == 'addedge':
-
+            self.runtime.add_edge(payload['graph'], payload['src'], payload['tgt'])
         elif command == 'removeedge':
-
+            self.runtime.remove_edge(payload['graph'], payload['src'], payload['tgt'])
         # IIP / literals
-        elif command == 'addiip':
-
-        elif command == 'removeiip':
-
-
-        # No support for exported in/out-ports
+        elif command == 'addinitial':
+            self.runtime.add_iip(payload['graph'], payload['tgt'], payload['src']['data'])
+        elif command == 'removeinitial':
+            self.runtime.remove_iip(payload['graph'], payload['tgt'])
+        # Exported ports
+        elif command in ('addinport', 'addoutport'):
+            pass # No support in this example
+        # Metadata changes
+        elif command in ('changenode'):
+            pass
         else:
+            send_ack = False
             print "WARN: Unknown command '%s' for protocol '%s' " % (command, 'graph')
 
+        # For any message we respected, send same in return as acknowledgement
+        if send_ack:
+            self.send('graph', command, payload)
 
 
     def handle_network(self, command, payload):
+        pass
+        # TODO
 
-        # 
-
-r = geventwebsocket.Resource({'/': RuntimeApplication}
-s = geventwebsocket.WebSocketServer(('', 8000), r)
-s.serve_forever()
+if __name__ == '__main__':
+    port = 3569
+    r = geventwebsocket.Resource({'/': RuntimeApplication})
+    s = geventwebsocket.WebSocketServer(('', port), r)
+    print 'INFO: Runtime listening on port %d' % (port,)
+    s.serve_forever()
